da95b1326ab21b1ae397b32b9cc0d574bb360fbe
Joe Mansour <jsmnsr@gmail.com>
2026-09-24 21:29:28 -0400

    test: Preserve output comparison after process exit

--- bin/run_test.py
+++ bin/run_test.py
@@ -233,8 +233,6 @@ def main():
 
         if exitcode:
             print("ERROR: Exited with %d '%s'" % (exitcode, input_fn))
-            nb_fail += 1
-            continue
 
         results, nb_invalid = parse_results(rtl433out, ignore_fields, expected_data, false_positives)
         nb_fail += nb_invalid
@@ -257,6 +255,8 @@ def main():
                     print("  * %s" % detail)
             print(" Expected: " + str(expected_data))
             print("  But got: " + str(results))
+        elif exitcode:
+            nb_fail += 1
         else:
             nb_ok += 1
 
